e0f5fc546cdda0479b2602f3ea83b53c2ac5f125
Josiah Khor <josiah.khor@tenjin.com.au>
2018-12-13 15:29:02 +1100

    draw portrait

--- render.py
+++ render.py
@@ -2,26 +2,57 @@ import matplotlib.pyplot as plt
 import numpy as np
 from math import cos, sin, pi
 
-def circle_points(r, n=100):
+black = '#000000'
+blue = '#0000ff'
+green = '#00b300'
+red = '#cc0000'
+a4_aspectratio = 1.414
+
+portrait1 = [
+    {
+        'color': blue,
+        'radius': 15
+        },
+    {
+        'color': black,
+        'radius': 25
+        },
+    {
+        'color': green,
+        'radius': 35
+        },
+    {   'color': red,
+        'radius': 45
+        }
+]
+
+def circle_points(r, n=75):
     return [(cos(2*pi/n*x)*r,sin(2*pi/n*x)*r, 2*pi/n*x) for x in range(0,n+1)]
 
-def line_points(t, r, l=10):
+def line_points(t, r, l=150):
     tt = t + pi/2
     p1 = (cos(t)*r + cos(tt)*l, sin(t)*r + sin(tt) * l)
     p2 = (cos(t)*r , sin(t)*r)
     p3 = (cos(t)*r - cos(tt)*l, sin(t)*r - sin(tt) * l)
     return [p1, p2, p3]
 
-circle = circle_points(5)
+def portrait(params):
+    for param in params:
+        circle = circle_points(param.get('radius'))
 
-x = [x for x, _ , _ in circle]
-y = [y for _, y, _ in circle]
+        x = [x for x, _ , _ in circle]
+        y = [y for _, y, _ in circle]
 
-plt.plot(x, y, 'o')
-for _, _, t in circle:
-    line = line_points(t, 5)
-    x = [x for x, _ in line]
-    y = [y for _, y in line]
-    plt.plot(x, y, '-')
-plt.axes().set_aspect('equal','datalim')
-plt.show()
+        for _, _, t in circle:
+            line = line_points(t, param.get('radius'))
+            x = [x for x, _ in line]
+            y = [y for _, y in line]
+            plt.plot(x, y, '-', color=param.get('color'), linewidth=0.2)
+    plt.axes().set_aspect('equal')
+    plt.axes().set_xlim(right=55, left=-55)
+    plt.axes().set_ylim(top=55*a4_aspectratio,bottom=-55*a4_aspectratio)
+    plt.axis('off')
+    #plt.show()
+    plt.savefig("test.png", bbox_inches='tight', pad_inches = 0, dpi=480)
+
+portrait(portrait1)
